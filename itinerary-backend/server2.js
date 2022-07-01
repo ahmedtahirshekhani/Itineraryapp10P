ccaa191eeec61689396eafa3ae751b189091524d
Ahmed Tahir Shekhani <ahmed.tahir@10pearls.com>
2022-07-01 10:21:30 +0500

    name and email added to user register api

--- itinerary-backend/server2.js
+++ itinerary-backend/server2.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const app = express();
-const port = 3000;
+const port = 3001;
 var cors = require("cors");
 
 const mongoose = require("mongoose");
@@ -18,12 +18,28 @@ const Locations = require("./models/locations");
 const Trips = require("./models/trips");
 const SingleTrip = require("./models/singletrip");
 
-
 app.use(cors());
+
+// var currentDate = new Date(); //use your date here
+
+
+// Date.prototype.formatMMDDYYYY = function(){
+//   return (this.getDate()) + 
+//   "-" +  (this.getMonth() + 1) +
+//   "-" +  this.getFullYear();
+// }
+
+// var parts =currentDate.formatMMDDYYYY().split("-"),
+//     date = new Date(+parts[2], parts[1]-1, +parts[0]);
+// console.log(date.toString());
+
+
+
+// console.log(currentDate.formatMMDDYYYY().toString())
 
 app.use(
   session({
-    secret: "asdasd23123helloasdasddasdasdasd2e3234123",
+    secret: "asda2$3w3213askhlkjkldalkl1o8@#",
     saveUninitialized: false,
     resave: false,
   })
@@ -34,8 +50,8 @@ app.listen(port, () => {
 });
 
 app.post("/api/register", async (req, res) => {
-  const {name, email, username, password } = req.body;
-  const resp = await User.findOne({ username, email });
+  const { username, password } = req.body;
+  const resp = await User.findOne({ username });
   if (resp) {
     res.send({
       success: false,
@@ -45,12 +61,10 @@ app.post("/api/register", async (req, res) => {
   }
 
   const user = new User({
-    name,
-    email,
     username,
-    password
+    password,
   });
-  await user.save();
+  const result = await user.save();
   req.session.username = username;
   req.session.save();
   res.send({
@@ -61,7 +75,6 @@ app.post("/api/register", async (req, res) => {
 
 app.post("/api/login", async (req, res) => {
   const { username, password } = req.body;
-  console.log(username, password)
   const resp = await User.findOne({ username, password });
   console.log(resp);
 
@@ -95,27 +108,12 @@ app.get("/api/logout", (req, res) => {
 
 app.get("/api/mainlocations", async (req, res) => {
   const locationArray = [];
+  console.log("Api is successful");
   const resp = await Locations.find();
   if (resp) {
     // resp.map((value) => {
     //   locationArray.push(value.name);
     // });
-    res.send({
-      success: true,
-      message: resp,
-    });
-  } else {
-    res.send({
-      success: false,
-      message: "Api failed! Error",
-    });
-  }
-});
-
-
-app.get("/api/getallusers", async (req, res) => {
-  const resp = await User.find();
-  if (resp) {
     res.send({
       success: true,
       message: resp,
@@ -135,7 +133,7 @@ app.post("/api/sublocations", async (req, res) => {
   const resp = await Locations.findOne({name:selectedMainLocation});
   // console.log(resp.subPlaces)
   if (resp) {
-
+    
     res.send({
       success: true,
       message: resp.subPlaces
@@ -155,7 +153,7 @@ app.get("/api/mytrips", async (req, res)=>{
   req.session.save()
   const resp = await Trips.findOne({username:req.session.username});
   if (resp) {
-
+    
     res.send({
       success: true,
       data: resp
@@ -174,22 +172,22 @@ app.post("/api/mytrips/single", async (req, res) => {
   req.session.save()
 
   const {tripname: tripName} = req.body;
-
+  
 
   const singleTripDetails = await SingleTrip.findOne({username:req.session.username, tripname:tripName});
   const singleTripMetaData = await Trips.findOne({username:req.session.username});
   let obj = {}
   singleTripMetaData.tripdata.map(val=>{
     if(val.name == tripName){
-
+     
      obj.metaData = val
      obj.singleTripDetails = singleTripDetails
-
+   
     }
   })
   // console.log(singleTripDetails)
   if (singleTripDetails) {
-
+    
     res.send({
       success: true,
       message: obj
@@ -216,4 +214,3 @@ app.post("/api/updatetrip", async (req, res)=>{
   })
 
 })
-
