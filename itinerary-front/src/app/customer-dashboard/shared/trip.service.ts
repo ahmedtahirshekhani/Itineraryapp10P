87c0d76b3cdfae51769044f272c0696c5afdc945
Fahad Shaikh <fahad.shaikh@10pearls.com>
2022-07-19 16:49:54 +0500

    Removed redundant/un-unsed code
    Removed interfaces that were not used and in addition, made the code cleaner.

--- itinerary-front/src/app/customer-dashboard/shared/trip.service.ts
+++ itinerary-front/src/app/customer-dashboard/shared/trip.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
+
 interface tripData {
   [x: string]: any;
   success: Boolean;
   data: [Object];
 }
 
-interface additionStatus {
-  success: Boolean;
-}
 interface tripAdded {
   success: Boolean;
   tripId: string;
